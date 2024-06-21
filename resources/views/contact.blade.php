c2c41447b32f5b984737a61691f154189151830a
Nur Fatchurohman <nur.fatchurohman@salt.co.id>
2024-06-21 15:49:29 +0700

    add tailwind and alpine js

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,15 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
+    <title>Contact</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Halaman About</h1>
-    <h3>Owner: {{ $name }}</h3>
     <a href="/">Home</a>
     <a href="/about">About</a>
     <a href="/blog">Blog</a>
     <a href="/contact">Contact</a>
+
+    <h1>Contact</h1>
+    <ul>
+        <li><h3>Nur Fatchurohman</h3></li>
+        <li><h5>Fullstact Developery</h5></li>
+        <li><a href="instagram.com">Instagram</a></li>
+    </ul>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['name' => 'Nur Fatchurohman']);
 });
 
 Route::get("/about", function() {
-    return view('about');
+    return view('about', ['name' => 'Nur Fatchurohman']);
+});
+
+Route::get('/blog', function() {
+    return view('blog', ['name' => 'Nur Fatchurohman']);
+});
+
+Route::get('/contact', function() {
+    return view('contact');
 });
