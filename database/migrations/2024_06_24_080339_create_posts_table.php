4f7d6a506a2a3c39f3a1477afe22199ca817fd48
Nur Fatchurohman <nur.fatchurohman@salt.co.id>
2024-06-24 16:33:01 +0700

    add migration

--- database/migrations/2024_06_24_080339_create_posts_table.php
+++ database/migrations/2024_06_24_080339_create_posts_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('slug')->unique();
+            $table->string('body');
             $table->timestamps();
+
         });
     }
 
