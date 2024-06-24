bbe4fc2ed33a785fc1529451e5cf2e9007b06e64
FaturFawkes <nur.faturohman28@gmail.com>
2024-06-24 21:57:09 +0700

    create relation in eloquent

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,13 +3,15 @@
 
     @foreach ($posts as $post)
         <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/posts/{{  $post['slug'] }}">
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:text-blue-600">{{ $post['title'] }}</h2>
+            <a href="/posts/{{ $post['slug'] }}">
+                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 hover:text-blue-500">{{ $post['title'] }}
+                </h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->format('j F Y') }}
+                <a class="hover:text-blue-500" href="/authors/{{ $post->author->id }}">{{ $post->author->name }} </a> |
+                {{ $post->created_at->format('j F Y') }}
             </div>
-            <p class="my-4 font-light">{{ Str::limit($post['body'], 50) }}</p>
+            <p class="my-4 font-light">{{ Str::limit($post->body, 50) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500">Read More &raquo;</a>
         </article>
     @endforeach
